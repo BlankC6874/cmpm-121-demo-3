4356d388563dc753f34053c9469caa34e343d41b
Guangyang Chen <1019951628@qq.com>
2024-11-04 05:21:42 +0000

    L16 Dialog finished: Added a button to see a message

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.createElement("button");
+  button.textContent = "Click me";
+  button.addEventListener("click", () => {
+    alert("you clicked the button!");
+  });
+  document.body.appendChild(button);
+});
